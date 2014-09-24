0dda14fa8f9f9325af2201edd625eed110ca9f44
Michael Brown <mbrown1413@gmail.com>
2014-09-23 23:23:42 -0400

    Docs for the Puyo board

--- puyo/board.py
+++ puyo/board.py
@@ -15,6 +15,7 @@ VALID_CELLS = (
     b'k',  # Black
 )
 
+# Colors that are drawn for each bean in the `draw()` method
 CELL_COLORS = {
     b' ': (255, 255, 255),
     b'r': (0, 0, 255),
@@ -39,15 +40,62 @@ def _validate_board(board, width, height):
 
 
 class Puyo1Board(object):
+    """One player's board of the Puyo game.
+
+    The board is represented by a 2 dimensional array of beans, 6 wide and 12
+    tall, stored in the `board` attribute. The origin is bottom left. Access it
+    like this:
+
+        puyo_board = Puyo1Board()
+        puyo_board.board[x][y]
+
+    Each cell is one of the following ASCII characters:
+
+        b' ': Nothing
+        b'r': Red
+        b'g': Green
+        b'b': Blue
+        b'y': Yellow
+        b'p': Purple
+        b'k': Black (nuisance)
+
+    The usual way for manipulating the board is through the method
+    `make_move()`. There are also some lower level methods which allow for
+    moves which could be invalid in an actual game. A typical AI would create a
+    list of moves using `iter_moves()`, and return the move it thinks is best.
+
+    The next beans are also stored, in the attribute `next_beans`. If given,
+    it's also shown is the image representation returned by `draw()`.
+
+    """
 
     def __init__(self, board=None, next_beans=None):
+        """
+        If `board` is given, it should be an length 6 array where each item is
+        a 12 length array. Create one like this:
+
+            board = [[blah for y in range(12)]
+                           for x in range(6)]
+
+        The given board will be copied upon initialization, so don't worry
+        about mutability.
+
+        If `next_beans` is given, it should be a list of 2 bean colors (black,
+        or nuisance beans aren't allowed).
+
+        """
         if board is None:
+            #TODO: This should really be a numpy array
             self.board = [[b' ' for y in range(12)]
                                  for x in range(6)]
         else:
             _validate_board(board, 6, 12)
             self.board = deepcopy(board)
 
+        if next_beans is not None:
+            assert len(next_beans) == 2
+            for bean in next_beans:
+                assert bean in (b'r', b'g', b'b', b'y', b'p')
         self.next_beans = next_beans
         self.game_over = False
 
@@ -93,7 +141,11 @@ class Puyo1Board(object):
     def drop_beans(self, xs, colors):
         """Drops one or more beans from the top of the board simultaneously.
 
-        TODO: Calculate score and return a namedtuple.
+        This method handles gravity, bean elimination and combos. If you're
+        actually playing a game, you should use `make_move()`, which is a bit
+        higher level since it only allows actually valid moves.
+
+        TODO: Return namedtuple of (score, n_eliminated)
 
         """
         for x, color in zip(xs, colors):
@@ -107,6 +159,27 @@ class Puyo1Board(object):
         self._drop(x, b'k')
 
     def make_move(self, beans, orientation, position):
+        """Drop a pair of beans as a part of a move in the game.
+
+        This is the method that should be used when actually playing the game,
+        as it offers the highest level of abstraction and disallows anything
+        illegal in a real game.
+
+        `beans` is a tuple of 2 bean colors. The first item is the top bean,
+        the second is the bottom. `orientation` is an integer 0-3 inclusive,
+        indicating how many times the pair of beans has been rotated 90 degrees
+        clockwise. `position` is an x position on the board, with 0 the
+        leftmost position the pair can occupy. If `orientation` is 0 or 2,
+        `position` must be between 0-5 inclusive, otherwise it must be between
+        0-4 inclusive.
+
+        If an invalid move is given, False is returned. Note that no moves are
+        valid if there is a game over. To check if a move is valid beforehand,
+        use `can_make_move()`.
+
+        TODO: Return score results from `drop_beans()`.
+
+        """
         assert len(beans) == 2
         for bean in beans:
             assert bean in (b'r', b'g', b'b', b'y', b'p')
@@ -133,6 +206,7 @@ class Puyo1Board(object):
         return True
 
     def can_make_move(self, orientation, position):
+        """Return True if the move can be made, False otherwise."""
         if self.game_over:
             return False
 
@@ -159,12 +233,23 @@ class Puyo1Board(object):
         return True
 
     def iter_moves(self):
+        """
+        Return an iterable of possible moves, as a list of (orientation,
+        position) tuples.
+        """
         for orientation in range(4):
             for position in range(5 if orientation%2 else 6):
                 if self.can_make_move(orientation, position):
                     yield orientation, position
 
     def is_game_over(self):
+        """Has the game been lost?
+
+        The game is lost when the third column is filled and the player is
+        forced to drop the next pair of beans above the board. This happens in
+        `make_move()`.
+
+        """
         return self.game_over
 
     def _drop(self, x, color):
