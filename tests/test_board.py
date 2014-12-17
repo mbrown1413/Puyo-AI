1677711ce37934580abcfdcb88ae39699e554677
Michael Brown <mbrown1413@gmail.com>
2014-12-16 23:12:48 -0500

    Test PuyoBoard.can_make_move()
    The test fails at the end because of a known edge case, where there isn't
    enough room to rotate a pair of beans.

---
+++ tests/test_board.py
@@ -0,0 +1,98 @@
+
+import unittest
+
+import puyo
+
+
+class TestPuyoBoard(unittest.TestCase):
+
+    def make_board(self, positions, colors):
+        b = puyo.PuyoBoard()
+        assert len(positions) == len(colors)
+        b.drop_beans(positions, colors)
+        return b
+
+    def test_can_make_move(self):
+        # Each test vector is a tuple of:
+        #
+        #     ((positions, colors), position, rotation, expected result)
+        #
+        # First, beans are dropped at the corresponding bean positions with
+        # the given colors given in the first item of the tuple. Then
+        # `PuyoBoard.can_make_move()` is called with the position and rotation
+        # given. The expected result is then compared with the actual result.
+        tests = []
+
+        # Nothing filled, every move should work
+        b = [], []
+        tests.extend([(b, pos, rot, True) for pos in range(5)
+                                          for rot in range(4)])
+        tests.extend([(b, 5, rot, True) for rot in (0, 2)])
+
+        # First column filled up
+        b = [0]*12, [b'r', b'g']*6
+        tests.extend([
+            (b, 0, 0, False),
+            (b, 0, 1, False),
+            (b, 0, 2, False),
+            (b, 0, 3, False),
+            (b, 1, 0, True),
+            (b, 1, 1, True),
+            (b, 1, 2, True),
+            (b, 1, 3, True),
+        ])
+
+        # One empty space in first column
+        b = [0]*11, [[b'r', b'g'][i%2] for i in range(11)]
+        tests.extend([
+            (b, 0, 0, True),
+            (b, 0, 1, True),
+            (b, 0, 2, True),
+            (b, 0, 3, True),
+        ])
+
+        # First column blocked off by second
+        b = [1]*12, [b'r', b'g']*6
+        tests.extend([(b, 0, r, False) for r in range(4)])
+        tests.extend([(b, 1, r, False) for r in range(4)])
+        tests.extend([(b, 2, r, True) for r in range(4)])
+
+        # Sixth column blocked off by fifth
+        b = [4]*12, [b'r', b'g']*6
+        tests.extend([(b, 5, r, False) for r in (0, 2)])
+        tests.extend([(b, 4, r, False) for r in range(4)])
+        tests.extend([(b, 3, r, r%2 == 0) for r in range(4)])
+
+        # No room to rotate when starting pos is surrounded by filled columns
+        b = [1]*12 + [3]*12, [b'r', b'g']*12
+        tests.extend([
+            (b, 2, 0, True),
+            (b, 2, 1, False),
+            (b, 2, 2, False),
+            (b, 2, 3, False),
+        ])
+
+        for i, vector in enumerate(tests):
+            beans, pos, rot, expected_result = vector
+            
+            positions, colors = beans
+            board = self.make_board(positions, colors)
+
+            result = board.can_make_move(pos, rot)
+            self.assertEqual(result, expected_result,
+                "Test vector {} ({}) did not match expected result.".format(i, vector)
+            )
+
+    @unittest.skip("Test not written")
+    def test_make_move(self):
+        # Make sure the game mechanics work correctly in make_move
+        pass  #TODO
+
+    @unittest.skip("Test not written")
+    def test_scoring(self):
+        # Make sure the correct combo is returned from make_move
+        pass  #TODO
+
+
+if __name__ == "__main__":
+    unittest.main()
