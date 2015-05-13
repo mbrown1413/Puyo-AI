85b875fb2b0e1da86107210d963e1ec9bd8db669
Michael Brown <mbrown1413@gmail.com>
2015-05-13 10:41:06 -0400

    Fix issues testing with C acceleration

--- tests/test_board.py
+++ tests/test_board.py
@@ -4,22 +4,31 @@ import unittest
 
 import puyo
 
-from helper import board_from_strs, PuyoTestCase
+from helper import PuyoTestCase
 
 
 class TestBoard(PuyoTestCase):
 
-    def get_empty_board(self):
-        return puyo.Board(c_accelerated=False)
+    def make_board(self, *args, **kwargs):
+        return puyo.Board(*args, c_accelerated=False, **kwargs)
 
-    def make_board(self, positions, colors):
-        b = self.get_empty_board()
+    def make_board_from_drops(self, positions=(), colors=()):
+        b = self.make_board()
         assert len(positions) == len(colors)
         b.drop_beans(positions, colors)
         return b
 
+    def board_from_strs(self, rows, next_beans=None):
+
+        for i in range(12-len(rows)):
+            rows.insert(0, b"      ")
+
+        board = [[rows[11-y][x] for y in range(12)]
+                                for x in range(6)]
+        return self.make_board(board, next_beans)
+
     def test_board_mutation(self):
-        board = puyo.Board()
+        board = self.make_board()
         self.assertEquals(board[1][1], b' ')
 
         board[1][1] = b'r'
@@ -100,7 +109,7 @@ class TestBoard(PuyoTestCase):
             beans, pos, rot, expected_result = vector
 
             positions, colors = beans
-            board = self.make_board(positions, colors)
+            board = self.make_board_from_drops(positions, colors)
 
             result = board.can_make_move(pos, rot)
             self.assertEqual(result, expected_result,
@@ -109,7 +118,7 @@ class TestBoard(PuyoTestCase):
 
     def test_combo1(self):
         """Simple length 1 combo mechanics."""
-        board = board_from_strs([
+        board = self.board_from_strs([
             b"rr r  ",
         ])
         combo = board.drop_bean(2, b'r')
@@ -120,7 +129,7 @@ class TestBoard(PuyoTestCase):
 
     def test_combo2(self):
         """Simple length 1 combo mechanics."""
-        board = board_from_strs([
+        board = self.board_from_strs([
             b"   r  ",
             b"rrrggg",
         ])
@@ -132,7 +141,7 @@ class TestBoard(PuyoTestCase):
 
     def test_nuisance_elimination(self):
         """Nuisance beans should be eliminated when next to a combo."""
-        board = board_from_strs([
+        board = self.board_from_strs([
             b"rr rk ",
         ])
         combo = board.drop_bean(2, b'r')
@@ -143,7 +152,7 @@ class TestBoard(PuyoTestCase):
 
     def test_nuisance_no_combo(self):
         """Four nuisance beans should not make a combo."""
-        board = board_from_strs([
+        board = self.board_from_strs([
             b"kk    ",
             b"kk    ",
         ])
@@ -151,26 +160,26 @@ class TestBoard(PuyoTestCase):
         self.assertEquals(combo.score, 0)
         self.assertEquals(combo.n_beans, 0)
         self.assertEquals(combo.length, 0)
-        self.assertBoardEquals(board, board_from_strs([
+        self.assertBoardEquals(board, self.board_from_strs([
             b"kk    ",
             b"kkg   ",
         ]))
 
     def test_nuisance_no_combo_diagonal(self):
         """Nuisance beans not should be eliminated when diagonal to a combo."""
-        board = board_from_strs([
+        board = self.board_from_strs([
             b"    k ",
             b"rr rg ",
         ])
         combo = board.drop_bean(2, b'r')
-        self.assertBoardEquals(board, board_from_strs([
+        self.assertBoardEquals(board, self.board_from_strs([
             b"    k ",
             b"    g ",
         ]))
 
     def test_long_combo(self):
         """Chain length 18!"""
-        board = board_from_strs([
+        board = self.board_from_strs([
             b" pyybg",
             b"bbppyg",
             b"bpyrbb",
@@ -201,39 +210,42 @@ class TestBoard(PuyoTestCase):
         self.assertEquals(combo.score, 440280)  # This is a puyo puyo (1)
 
     def test_drop_nuisance_1(self):
-        board = puyo.Board()
+        board = self.make_board()
         board.drop_nuisance(1)
         self.assertEqual(board.count(b'k'), 1)
 
     def test_drop_nuisance_6(self):
-        board = puyo.Board()
+        board = self.make_board()
         board.drop_nuisance(6)
-        self.assertBoardEquals(board, board_from_strs([b'kkkkkk']))
+        self.assertBoardEquals(board, self.board_from_strs([b'kkkkkk']))
 
     def test_drop_nuisance_7(self):
-        board = puyo.Board()
+        board = self.make_board()
         board.drop_nuisance(7)
         self.assertEqual(board.count(b'k'), 7)
         for x in range(6):
             self.assertEqual(board[x][0], b'k')
 
     def test_drop_nuisance_36(self):
-        board = puyo.Board()
+        board = self.make_board()
         board.drop_nuisance(36)
-        self.assertBoardEquals(board, board_from_strs([b'kkkkkk']*6))
+        self.assertBoardEquals(board, self.board_from_strs([b'kkkkkk']*6))
 
     def test_drop_nuisance_73(self):
         """Nuisance beans dropped over the top should be ignored."""
-        board = puyo.Board()
+        board = self.make_board()
         board.drop_nuisance(73)
-        self.assertBoardEquals(board, board_from_strs([b'kkkkkk']*12))
+        self.assertBoardEquals(board, self.board_from_strs([b'kkkkkk']*12))
 
 
 class TestBoardCAccelerated(TestBoard):
     """Test `puyo.Board` with C acceleration."""
 
-    def get_empty_board(self):
-        return puyo.Board(c_accelerated=True)
+    def make_board(self, *args, **kwargs):
+        board = puyo.Board(*args, c_accelerated=True, **kwargs)
+        if not board.c_accelerated:
+            raise RuntimeError("C acceleration not available, cannot test it.")
+        return board
 
 
 if __name__ == "__main__":
