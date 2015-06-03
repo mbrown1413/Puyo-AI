16f2020b0074d59b9e79e7350396c08a59ec5dc2
Michael Brown <mbrown1413@gmail.com>
2015-06-03 19:11:53 -0400

    Support more than one button press in gc_send.py

--- gc_send.py
+++ gc_send.py
@@ -16,36 +16,25 @@ In the case of a Puyo move, it is executed as fast as possible.
 
 """
 
+from time import sleep
+
 from puyo import GamecubeController
 
-def main():
-    import argparse
-    parser = argparse.ArgumentParser(description=__doc__,
-        formatter_class=argparse.RawDescriptionHelpFormatter)
-    parser.add_argument("gc_dev", help='Serial device for the Arduino '
-        'Gamecube controller. Example: "/dev/ttyACM0" or "/dev/ttyUSB0".')
-    parser.add_argument("command", help='Either the name of a '
-        'button/movement, or a puyo move specification (a column and a '
-        'rotation separated by a comma with no space, eg. "0,3"). Valid '
-        'buttons are: {}'.format(GamecubeController.BUTTON_BITS.keys()))
-    args = parser.parse_args()
-
-    comma_count = args.command.count(',')
+def execute_command(controller, command, errorfunc=lambda x: None):
+    comma_count = command.count(',')
 
     # Button Press
     if comma_count == 0:
-        args.command = args.command.lower()
-        if args.command not in GamecubeController.BUTTON_BITS:
-            parser.error('Invalid button "{}". Valid buttons are: '
-                    '{}'.format(args.command,
-                                GamecubeController.BUTTON_BITS.keys()))
-        controller = GamecubeController(args.gc_dev)
-        controller.push_button(args.command)
+        command = command.lower()
+        if command not in GamecubeController.BUTTON_BITS:
+            errorfunc('Invalid button "{}". Valid buttons are: '
+                      '{}'.format(command, GamecubeController.BUTTON_BITS.keys()))
+        controller.push_button(command)
 
     # Puyo Move
     elif comma_count == 1:
 
-        def validate(parser, name, min_x, max_x, x):
+        def validate(name, min_x, max_x, x):
             error = False
             try:
                 x = int(x)
@@ -54,19 +43,37 @@ def main():
             if x < min_x or x > max_x:
                 error = True
             if error:
-                parser.error('{} must be an integer between {} and {} '
+                errorfunc('{} must be an integer between {} and {} '
                         'inclusive, not "{}"'.format(name, min_x, max_x, x))
                 raise RuntimeError()
             return x
 
-        controller = GamecubeController(args.gc_dev)
-        col, rot = args.command.split(',', 1)
-        col = validate(parser, "Column", 0, 6, col)
-        rot = validate(parser, "Rotation", -3, 3, rot)
+        controller = GamecubeController(gc_dev)
+        col, rot = command.split(',', 1)
+        col = validate("Column", 0, 6, col)
+        rot = validate("Rotation", -3, 3, rot)
         controller.puyo_move(col, rot)
 
     else:
-        parser.error("Error parsing command argument: Too many commas")
+        errorfunc("Error parsing command argument: Too many commas")
+
+def main():
+    import argparse
+    parser = argparse.ArgumentParser(description=__doc__,
+        formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument("gc_dev", help='Serial device for the Arduino '
+        'Gamecube controller. Example: "/dev/ttyACM0" or "/dev/ttyUSB0".')
+    parser.add_argument("command", nargs='+', help='Either the name of a '
+        'button/movement, or a puyo move specification (a column and a '
+        'rotation separated by a comma with no space, eg. "0,3"). Valid '
+        'buttons are: {}'.format(GamecubeController.BUTTON_BITS.keys()))
+    args = parser.parse_args()
+
+    controller = GamecubeController(args.gc_dev)
+    for command in args.command:
+        execute_command(controller, command, parser.error)
+        sleep(0.05)
+
 
 if __name__ == "__main__":
     main()
